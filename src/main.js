3fc1c11ed824e249096bc2da3add0b4fb056a378
volnyagin <vconst@users.noreply.github.com>
2022-12-11 22:05:11 +0300

    add addPlayer event

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 var socket = io('https://lm-game-server.vconst.repl.co');
 
+const playerIndex = Math.floor(Math.random() * 10000);
+
 socket.on('connect', function() {
   socket.emit('addPlayer', {
-    playerName: 'test'
+    playerName: playerIndex.toString()
   });
+});
+
+socket.on('addPlayer', function(player) {
+   console.log(addPlayer, player);
 });
