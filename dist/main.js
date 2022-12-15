2cb0e12ca907457f2f0cc733bf87a1c5b35daa2a
volnyagin <vconst@users.noreply.github.com>
2022-12-15 16:16:11 +0300

    player movement

--- dist/main.js
+++ dist/main.js
@@ -10,8 +10,8 @@
       playerName: playerIndex.toString()
     });
   });
-  socket.on("addPlayer", function(player) {
-    console.log("addPlayer", player.playerName);
+  socket.on("addPlayer", function(player2) {
+    console.log("addPlayer", player2.playerName);
   });
 
   // node_modules/kaboom/dist/kaboom.mjs
@@ -2932,9 +2932,54 @@ vec4 frag(vec3 pos, vec2 uv, vec4 color, sampler2D tex) {
 
   // src/main.js
   var k = ao({ global: false });
+  k.focus();
   k.loadBean();
-  k.add([
-    k.sprite("bean")
+  var background = k.add([
+    k.scale(1),
+    k.fixed()
   ]);
+  background.onDraw(() => {
+    k.drawRect({
+      width: k.width(),
+      height: k.height(),
+      fill: true,
+      color: k.Color.fromArray([200, 200, 200])
+    });
+  });
+  k.onDraw(() => {
+    k.drawText({
+      text: (60 - k.time()).toFixed(),
+      size: 30,
+      font: "sink",
+      pos: k.vec2(k.width() - 70, 20)
+    });
+  });
+  var player = k.add([
+    k.sprite("bean"),
+    k.pos(100, 100)
+  ]);
+  var movePlayer = /* @__PURE__ */ __name((direction) => {
+    const speed = 200;
+    if (direction === "up") {
+      player.move(0, -speed);
+    }
+    if (direction === "down") {
+      player.move(0, speed);
+    }
+    if (direction === "left") {
+      player.move(-speed, 0);
+    }
+    if (direction === "right") {
+      player.move(speed, 0);
+    }
+  }, "movePlayer");
+  k.onKeyDown("w", () => movePlayer("up"));
+  k.onKeyDown("up", () => movePlayer("up"));
+  k.onKeyDown("s", () => movePlayer("down"));
+  k.onKeyDown("down", () => movePlayer("down"));
+  k.onKeyDown("d", () => movePlayer("right"));
+  k.onKeyDown("right", () => movePlayer("right"));
+  k.onKeyDown("a", () => movePlayer("left"));
+  k.onKeyDown("left", () => movePlayer("left"));
 })();
 //# sourceMappingURL=main.js.map
--- src/main.js
+++ src/main.js
@@ -4,9 +4,61 @@ import kaboom from 'kaboom';
 
 const k = kaboom({ global: false });
 
+k.focus();
+
 
 k.loadBean()
 
-k.add([
+const background = k.add([
+	k.scale(1),
+	k.fixed()
+]);
+
+background.onDraw(() => {
+	k.drawRect({
+		width: k.width(),
+		height: k.height(),
+		fill: true,
+		color: k.Color.fromArray([200, 200, 200]),
+	})
+});
+
+
+k.onDraw(() => {
+	k.drawText({
+		text: (60 - k.time()).toFixed(),
+		size: 30,
+		font: "sink",
+		pos: k.vec2(k.width() - 70, 20)
+	})
+})
+
+const player = k.add([
 	k.sprite("bean"),
+	k.pos(100, 100),
 ])
+
+const movePlayer = (direction) => {
+	const speed = 200;
+	if(direction === 'up') {
+		player.move(0, -speed);
+	}
+	if(direction === 'down') {
+		player.move(0, speed);
+	}
+	if(direction === 'left') {
+		player.move(-speed, 0);
+	}
+	if(direction === 'right') {
+		player.move(speed, 0);
+	}
+}
+
+k.onKeyDown('w', () => movePlayer('up'));
+k.onKeyDown('up', () => movePlayer('up'));
+k.onKeyDown('s', () => movePlayer('down'));
+k.onKeyDown('down', () => movePlayer('down'));
+k.onKeyDown('d', () => movePlayer('right'));
+k.onKeyDown('right', () => movePlayer('right'));
+k.onKeyDown('a', () => movePlayer('left'));
+k.onKeyDown('left', () => movePlayer('left'));
