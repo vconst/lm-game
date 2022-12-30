44855d4d2e5765ccdfceda6aed13505b790c620e
volnyagin <vconst@users.noreply.github.com>
2022-12-30 10:49:45 +0300

    return old ws server

--- dist/main.js
+++ dist/main.js
@@ -414,7 +414,7 @@
   });
 
   // src/socket.js
-  var socket = io("http://91.210.171.100:4444");
+  var socket = io("https://lm-game-server.vconst.repl.co");
   var socket_default = {
     on: socket.on.bind(socket),
     off: socket.off.bind(socket),
